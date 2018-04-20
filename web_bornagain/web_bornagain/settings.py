181858e20d66f70c45ebf084f86beb89a180867d
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-04-20 13:35:14 +0200

    Replaced SQLite with PostgreSQL

--- web_bornagain/web_bornagain/settings.py
+++ web_bornagain/web_bornagain/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'web_bornagain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'web_ba_polls',
+        'USER': 'web_ba_test',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
